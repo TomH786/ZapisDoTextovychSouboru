13b9b11ca3fdf479a2d1889a943d0d7235dde44c
TomH786 <hortom2002@gmail.com>
2024-12-06 13:07:23 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        try(Scanner scanner = new Scanner(new BufferedReader(new FileReader("text.txt")))) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("Soubor nebyl nalezen");
+        }
+
+        try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("text.txt")))) {
+            writer.println("Hello world!");
+        }catch (IOException e){
+            System.out.println("Chyba zápisu do souboru");
+        }
 
 
     }
